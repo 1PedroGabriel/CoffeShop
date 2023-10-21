de6d1a62a6699ece33ada755ce15ed725b529685
PKSLZ <peneloyerock@gmail.com>
2023-10-20 21:23:21 -0300

    menu feito

--- app/Http/Controllers/menuController.php
+++ app/Http/Controllers/menuController.php
@@ -2,12 +2,43 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Models\Produto;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class menuController extends Controller
 {
     public function index(){
-        return view("menu");
+
+        $produtos = Produto::all();
+        return view("menu")
+                ->with('produtos', $produtos);
     }
+
+    public function create(Request $request){
+
+        $request->validate([
+            'nome' => ['required'],
+            'categoria' => ['required'],
+            'preco' => ['required'],
+        ]);
+
+        $newProduct = $request->only(['nome', 'categoria', 'preco']);
+        
+        Produto::create($newProduct);
+        
+        return redirect('/menu');
+    }
+    public function show(Request $request){
+
+
+        $produto = Produto::all()->where('id', $request->id)->first();
+        
+      
+        return view('menuShow')
+                    ->with('produto',$produto);
+          
+    
+    }
+
 }
